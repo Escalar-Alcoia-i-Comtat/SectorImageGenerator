9736970beb9893589f7b72c7cf47c0dc4f8a0716
Arnau Mora <arnyminer.z@gmail.com>
2021-04-26 18:22:12 +0200

    :sparkles: Added grade coloring.

--- www/js/firebase.js
+++ www/js/firebase.js
@@ -46,6 +46,37 @@ if (area != null && zone != null && sector != null)
                     return (stringsCount > 0 ? count : "") + `<i class="eaic-icon ${icon}"></i>`
                 return ""
             }
+            const getGradeColor = function (grade) {
+                if (grade.substr(0, 1) === "L")
+                    return this(grade.substr(3));
+
+                let gradeColor = "#222222";
+                if (grade[0] === "A")
+                    gradeColor = "#dbde26";
+                else if (grade[0] === "8")
+                    gradeColor = "#000000";
+                else if (grade[0] === "7")
+                    gradeColor = "#d30b1d";
+                else if (grade[0] === "6")
+                    gradeColor = "#4b82db";
+                else if (grade[0] === "5" || grade[0] === "4" || grade[0] === "3")
+                    gradeColor = "#2aa349";
+                return gradeColor;
+            }
+            const processGrade = function (grade) {
+                let builder = "";
+                const pieces = grade.split("/");
+                for (let p = 0; p < pieces.length; p++)
+                    if (pieces.hasOwnProperty(p)) {
+                        const piece = pieces[p];
+                        const color = getGradeColor(piece)
+                        let obj = `<span style="color:${color}">${piece}</span>`;
+                        if (pieces.length - p > 1)
+                            obj += "/"
+                        builder += obj;
+                    }
+                return builder;
+            }
 
             const height = heightData.length > 0 ? heightData[0] + " m" : "";
             const ending = endingData.length > 0 ? `<span class="eaic-icon ${endingData[0]}"></span>` : "";
@@ -54,11 +85,12 @@ if (area != null && zone != null && sector != null)
                 safeDisplay(data.pitonCount, data.stringCount, "piton") +
                 safeDisplay(data.spitCount, data.stringCount, "spit") +
                 safeDisplay(data.tensorCount, data.stringCount, "tensor");
+            const processedGrade = processGrade(data.grade);
 
             contentTable.innerHTML += `<tr>
         <td>${data.sketchId}</td>
         <td>${data.displayName}</td>
-        <td>${data.grade}</td>
+        <td>${processedGrade}</td>
         <td>${safes}</td>
         <td>${height}</td>
         <td>${ending}</td>
