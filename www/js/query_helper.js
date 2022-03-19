0a0f99043373f26bf15250834847197a054ff0c7
Arnau Mora <arnyminer.z@gmail.com>
2022-03-19 12:10:53 +0100

    :sparkles: Added `getUrlParameters`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- www/js/query_helper.js
+++ www/js/query_helper.js
@@ -1,10 +1,18 @@
-function parse_query_string(query) {
+/**
+ * Fetches the GET parameters from the URL and returns them as a Dictionary.
+ * @author Arnau Mora
+ * @since 20220318
+ * @return {Object.<string, any>} A dictionary with keys the GET names, and values their corresponding values.
+ */
+const getUrlParameters = () => {
+    const query = window.location.search.substring(1);
     const vars = query.split("&");
     const query_string = {};
     for (let i = 0; i < vars.length; i++) {
         const pair = vars[i].split("=");
         const key = decodeURIComponent(pair[0]);
         const value = decodeURIComponent(pair[1]);
+
         // If first entry with this name
         if (typeof query_string[key] === "undefined")
             query_string[key] = decodeURIComponent(value);
@@ -16,4 +24,4 @@ function parse_query_string(query) {
             query_string[key].push(decodeURIComponent(value));
     }
     return query_string;
-}
+};
