5eabd27e8a894ccb54f11858ca65b07bea6b736a
Arnau Mora <arnyminer.z@gmail.com>
2022-03-27 21:41:23 +0100

    :sparkles: Added `constants.js`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ www/js/constants.js
@@ -0,0 +1,55 @@
+/**
+ * The color for all the grades of 5+ and lower.
+ * @author Arnau Mora
+ * @since 20220321
+ * @type {string}
+ */
+const GRADE_COLOR_5 = "#2aa349";
+
+/**
+ * The color for all the grades between 6a and 6c+
+ * @author Arnau Mora
+ * @since 20220321
+ * @type {string}
+ */
+const GRADE_COLOR_6 = "#4b82db";
+
+/**
+ * The color for all the grades between 7a and 7c+.
+ * @author Arnau Mora
+ * @since 20220321
+ * @type {string}
+ */
+const GRADE_COLOR_7 = "#d30b1d";
+
+/**
+ * The color for all the grades greater than 8a.
+ * @author Arnau Mora
+ * @since 20220321
+ * @type {string}
+ */
+const GRADE_COLOR_8 = "#000000";
+
+/**
+ * The color for all the grades marked as "artificial".
+ * @author Arnau Mora
+ * @since 20220321
+ * @type {string}
+ */
+const GRADE_COLOR_A = "#dbde26";
+
+/**
+ * The server hostname for the backend for data requests.
+ * @author Arnau Mora
+ * @since 20220327
+ * @type {string}
+ */
+const BACKEND_SERVER = "server.escalaralcoiaicomtat.org:3000";
+
+/**
+ * The http protocol to use for the backend server.
+ * @author Arnau Mora
+ * @since 20220327
+ * @type {string}
+ */
+const BACKEND_PROTOCOL = PROTOCOL_HTTP;
