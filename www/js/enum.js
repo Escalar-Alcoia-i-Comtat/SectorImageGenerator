0372e32c600675c03861f8c5564e62a057ecfe77
Arnau Mora <arnyminer.z@gmail.com>
2022-03-27 21:42:01 +0100

    :sparkles: Added `enum.js`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ www/js/enum.js
@@ -0,0 +1,15 @@
+/**
+ * The prefix for the HTTP protocol.
+ * @author Arnau Mora
+ * @since 20220327
+ * @type {string}
+ */
+const PROTOCOL_HTTP = "http://";
+
+/**
+ * The prefix for the HTTPS protocol.
+ * @author Arnau Mora
+ * @since 20220327
+ * @type {string}
+ */
+const PROTOCOL_HTTPS = "https://";
